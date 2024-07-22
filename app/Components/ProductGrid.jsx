f8ad81e17038d8a386a28ae9bf657bae12aae23f
MarcoDonna <marco.donna03@gmail.com>
2024-07-22 20:53:29 +0200

    improved layout of results

--- app/Components/ProductGrid.jsx
+++ app/Components/ProductGrid.jsx
@@ -2,7 +2,7 @@ import Product from "./Product"
 
 export default function ProductGrid({ products }){
   return (
-    <div>
+    <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-10">
       {
         products.map(product => <Product
           key={product.id}
@@ -10,6 +10,7 @@ export default function ProductGrid({ products }){
           price={product.price}
           rating={product.rating}
           image={product.thumbnail}
+          category={product.category}
         />)
       }
     </div>
