d5ec9937ea7d2c34fccf8857937bb18690b0ffbf
MarcoDonna <marco.donna03@gmail.com>
2024-07-22 20:46:03 +0200

    added tailwind back to the project

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Interview from "./Components/Interview"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main>
       <Interview/>
     </main>
   )
