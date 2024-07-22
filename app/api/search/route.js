2ec0c75c8a477fefd8958d4560e0e32438f920ba
MarcoDonna <marco.donna03@gmail.com>
2024-07-22 14:02:09 +0200

    added rating range to api query params

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,12 +1,17 @@
 import { NextResponse } from 'next/server';
 
-async function getProductData({ query, priceMin, priceMax }){
+async function getProductData({ query, priceMin, priceMax, ratingMin, ratingMax }){
   const response = await fetch("https://dummyjson.com/products/search?q=" + query);
   const productsData = await response.json();
 
-  // Filter products within the given price range.
+  // Filter products within the given price range and rating.
   return productsData.products.filter(product => {
-    return product.price >= priceMin && product.price <= priceMax;
+    return (
+      product.price >= priceMin
+      && product.price <= priceMax
+      && product.rating >= ratingMin
+      && product.rating <= ratingMax
+    );
   });
 }
 
@@ -17,8 +22,10 @@ export async function GET(req){
   const query = searchParams.get("q") || "";
   const priceMin = parseFloat(searchParams.get("priceMin")) || 0;
   const priceMax = parseFloat(searchParams.get("priceMax")) || Number.MAX_VALUE;
+  const ratingMin = parseFloat(searchParams.get("ratingMin")) || 0;
+  const ratingMax = parseFloat(searchParams.get("ratingMax")) || Number.MAX_VALUE;
 
-  const productData = await getProductData({ query, priceMin, priceMax });
+  const productData = await getProductData({ query, priceMin, priceMax, ratingMin, ratingMax });
 
   return NextResponse.json(productData);
 }
